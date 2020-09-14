d0fd2c3ecf1d232b99c1617f119f915d92d84556
jini-lee <zaddy.jini@gmail.com>
2020-09-14 22:53:58 +0900

    feature/apply_deployment

---
+++ deployment/README.md
@@ -0,0 +1,16 @@
+# Deployment
+
+## Apply deployment
+```
+$ kubectl apply -f deployment
+```
+
+## Status of deployment
+```
+$ kubectl get deployment simpleapp
+$ kubectl get po
+``` 
+
+
+
+
---
+++ deployment/simple_app.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import http.server
+
+
+class SimpleWebServer(http.server.BaseHTTPRequestHandler):
+    def do_GET(s):
+        s.send_response(200)
+        s.send_header('Content-Type', 'text/html')
+        s.end_headers()
+        s.wfile.write(b'''
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PYTHON SIMPLE PAGE!</title>
+    </head>
+    <body>
+        <p>This is a simple page!</p>
+    </body>
+</html>''')
+
+
+if __name__ == '__main__':
+    httpd = http.server.HTTPServer(('0.0.0.0', 8080), SimpleWebServer)
+    httpd.serve_forever()
